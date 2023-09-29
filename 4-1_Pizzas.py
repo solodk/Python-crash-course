c7d77e54eb5fab265cfae276f38705bb5795f4ba
solodk <solodk.m@gmail.com>
2023-09-29 15:10:27 +0300

    init commint

---
+++ 4-1_Pizzas.py
@@ -0,0 +1,9 @@
+# Think of at least three kinds of your favourite pizzas.
+# Store these pizza names in a list, and then use a for loop to print the name of each pizza.
+
+pizzas = ['peperoni', 'hamburger', 'four cheese']
+
+for pizza in pizzas:
+    print(f'I like {pizza.title()} pizza')
+
+print('I really love pizza!')
---
+++ 4-2_Animals.py
@@ -0,0 +1,14 @@
+# Think of at least three different animals that have a common characteristic.
+# Store the names of these animals in a list, and then use a for loop to print out the name of each animal
+
+animals = ['rabbit', 'jaguar', 'falcon']
+
+print(f'A {animals[0].title()} is very cute')
+print(f'A {animals[1].title()} is very fast')
+print(f'A {animals[2].title()} could be a pet')
+
+for animal in animals:
+    print(animal)
+
+print('All these animals are very fast')
+print('Test')
